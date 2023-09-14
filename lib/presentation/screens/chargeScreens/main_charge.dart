255bdf51adfa2a011c66a16bd36b8324acf50264
Esclender <elesclenderlugo@gmail.com>
2023-09-14 15:35:59 -0500

    refactor: MainCharge

--- lib/presentation/screens/chargeScreens/main_charge.dart
+++ lib/presentation/screens/chargeScreens/main_charge.dart
@@ -14,33 +14,36 @@ class EffectIntro extends StatefulWidget {
 class _EffectIntroState extends State<EffectIntro>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  double _scale = 1.0;
+  late Animation<double> _scale;
 
   @override
   void initState() {
     super.initState();
 
-    Future.delayed(const Duration(milliseconds: 1990), () {
+    Timer(const Duration(milliseconds: 1500), () {
       SystemChrome.setSystemUIOverlayStyle(SystemModifiers.overlayDark);
     });
 
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 3), // Adjust the duration as needed
+      duration:
+          const Duration(milliseconds: 2500), // Adjust the duration as needed
     );
 
-    _controller.forward(); // Start the animation
-
-    // Add a curve to the animation
-    final curvedAnimation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut, // Replace with your desired curve
+    // Create the scale animation
+    _scale = Tween<double>(begin: 1.0, end: 20.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut, // Replace with your desired curve
+      ),
     );
 
-    curvedAnimation.addListener(() {
-      setState(() {
-        _scale = curvedAnimation.value * 20.0;
-      });
+    // Start the animation
+    _controller.forward();
+
+    // Add a listener to the scale animation
+    _scale.addListener(() {
+      setState(() {});
     });
   }
 
@@ -49,10 +52,10 @@ class _EffectIntroState extends State<EffectIntro>
     return Container(
       color: c1,
       child: Stack(
-        alignment: AlignmentDirectional.center,
+        alignment: Alignment.center,
         children: [
           Transform.scale(
-            scale: _scale,
+            scale: _scale.value,
             child: Image.asset(
               'assets/images/start-vector.png',
               fit: BoxFit.contain,
@@ -72,17 +75,38 @@ class LogoAnimation extends StatefulWidget {
   State<LogoAnimation> createState() => _StartingState();
 }
 
-class _StartingState extends State<LogoAnimation> {
-  Alignment _alignment = Alignment.bottomCenter;
+class _StartingState extends State<LogoAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controllador;
+  late Animation<Alignment> _alignment;
+
+  //MANAGE THE ANIMATIONS WITH LISTENERS LIKE ABOVE XD
 
   @override
   void initState() {
     super.initState();
 
-    Timer(const Duration(seconds: 1), () {
-      setState(() {
-        _alignment = Alignment.center;
-      });
+    _controllador = AnimationController(
+      vsync: this,
+      duration:
+          const Duration(milliseconds: 100), // Adjust the duration as needed
+    );
+
+    // Create the alignment animation
+    _alignment = AlignmentTween(
+      begin: Alignment.bottomCenter,
+      end: Alignment.center,
+    ).animate(
+      CurvedAnimation(
+        parent: _controllador,
+        curve: Curves.fastOutSlowIn,
+      ),
+    );
+
+    _controllador.forward();
+
+    _alignment.addListener(() {
+      setState(() {});
     });
   }
 
@@ -91,7 +115,7 @@ class _StartingState extends State<LogoAnimation> {
     return Stack(
       children: [
         AnimatedAlign(
-          alignment: _alignment,
+          alignment: _alignment.value,
           duration: const Duration(seconds: 1),
           curve: Curves.fastOutSlowIn,
           child: Image.asset('assets/images/Logo.png'),
@@ -99,7 +123,7 @@ class _StartingState extends State<LogoAnimation> {
         Padding(
           padding: const EdgeInsets.only(top: 160.0),
           child: AnimatedAlign(
-            alignment: _alignment,
+            alignment: _alignment.value,
             duration: const Duration(seconds: 1),
             curve: Curves.fastOutSlowIn,
             child: Text(
