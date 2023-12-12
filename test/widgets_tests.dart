e11948b0cdb0f345e0fa6a3843bf786dbf06bfb7
Esclender <elesclenderlugo@gmail.com>
2023-12-12 17:40:57 -0500

    TEST WIDGETS 2

--- test/widgets_tests.dart
+++ test/widgets_tests.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:grassport_app/main.dart';
+import 'package:grassport_app/presentation/screens/StartingApp/location_agree.dart';
 import 'package:grassport_app/presentation/screens/chargeScreens/main_charge.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Widget createHomeScreen() => const BlocsProvider();
 
@@ -46,12 +48,12 @@ testBuscar(WidgetTester tester) async {
 
 void main() {
   group('Grassport Pruebas Widgets', () {
+    SharedPreferences.setMockInitialValues({});
+
     testWidgets('LogoAnimation bottom to Top', (tester) async {
       await tester.pumpWidget(createHomeScreen());
 
       await tester.runAsync(() async {
-        await Future.delayed(const Duration(seconds: 2));
-
         final Finder logoAnimationFinder = find.byType(LogoAnimation);
 
         expect(logoAnimationFinder, findsOneWidget);
@@ -63,26 +65,26 @@ void main() {
       });
     });
 
-    // testWidgets('Pantalla de carga aparece', (tester) async {
-    //   await tester.pumpWidget(createHomeScreen());
+    testWidgets('Pantalla de carga aparece', (tester) async {
+      await tester.pumpWidget(createHomeScreen());
 
-    //   await tester.runAsync(() async {
-    //     await testclickSaltar(tester);
+      await tester.runAsync(() async {
+        await testclickSaltar(tester);
 
-    //     final Finder emailInput = find.byKey(const Key('emailInput'));
-    //     final Finder claveInput = find.byKey(const Key('passwordInput'));
-    //     final Finder loginBtn = find.byKey(const Key('login'));
+        final Finder loginBtn = find.byKey(const Key("login_luego"));
+        await tester.tap(loginBtn);
 
-    //     await tester.tap(loginBtn);
-    //     await tester.enterText(emailInput, 'gilmer48mm.@gmail.com');
-    //     await tester.enterText(claveInput, '1234');
+        await tester.pumpAndSettle();
+        final Finder locationAgree = find.text('Permisos de ubicacion');
 
-    //     await tester.pumpAndSettle();
-    //     await tester.pumpAndSettle();
-    //     await tester.pumpAndSettle();
+        expect(locationAgree, findsOneWidget);
 
-    //     await testSesionManualmente(tester);
-    //   });
-    // });
+        await testSesionManualmente(tester);
+
+        final Finder logoAnimationFinder = find.byType(EffectIntro);
+
+        expect(logoAnimationFinder, findsOneWidget);
+      });
+    });
   });
 }
